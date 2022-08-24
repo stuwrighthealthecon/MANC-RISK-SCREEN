4386fbf5df741e99a0605be9113972b2c387ce94
Stuart Wright <stuart.j.wright@manchester.ac.uk>
2022-08-24 12:19:03 +0100

    Update MANC-RISK-SCREEN GAM Version 1.R

--- Model Core Files/MANC-RISK-SCREEN GAM Version 1.R
+++ Model Core Files/MANC-RISK-SCREEN GAM Version 1.R
@@ -18,7 +18,7 @@ library(devtools)
 # Load data -----------------------------------------------------------------------------------
 alt_names<-c("noscreening","procas","tertiles","3yr","2yr","5yr",
              "10yr","lowrisk5yr","lowrisk6yr","fullstrat")
-subD <- "GAM inputs"
+subD <- "GitHub/MANC-RISK-SCREEN/Model Core Files/GAM inputs"
 load(here(subD, "PSA_values.RData"))
 
 PSA_all <- tibble(strat = factor(alt_names)) %>%
