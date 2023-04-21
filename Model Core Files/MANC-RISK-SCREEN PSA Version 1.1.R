c514e3c4dcc276cf16bf93d78d549d3e6b64e012
Stuart Wright <stuart.j.wright@manchester.ac.uk>
2023-04-21 15:25:36 +0100

    Folder cleanup

--- Model Core Files/MANC-RISK-SCREEN PSA Version 1.1.R
+++ Model Core Files/MANC-RISK-SCREEN PSA Version 1.1.R
@@ -28,9 +28,6 @@ setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 numcores<-7
 registerDoParallel(cores=numcores)
 
-#Set timer to record duration of simulation
-ptm <- proc.time()
-
 #Load file containing required functions for the model
 source(file="MANC_RISK_SCREEN_functions Version 1.R")
 
@@ -301,38 +298,37 @@ if(gensample==1){
   #Set growth rate for tumours
   names(risksample)[4:14]<-paste(c("Risk Group","VDG","MRI Screening","US Screening","Risk Predicted","Feedback","Interval Change","Life Expectancy","Cancer","Clinical Detection Size","Growth Rate"))
   
-  #survmvn<-data.frame(c(-5.46208,-5.2077,-5.8016),c(-3.8163,-3.75901,-3.8811),c(-2.72264,-2.66053,-2.78617))
-  #survcovmat<-cov(survmvn)
-  #survmeans<-c(survmvn[1,1],survmvn[1,2],survmvn[1,3])
-  #PSA_gamma_survival<-mvrnorm(mcruns,survmeans,survcovmat)
+#########################Add Monte Carlo Draws into Sample##############  
   
-  PSA_gamma_survival<-c(rnorm(1,-5.46208,0.137),rnorm(1,-3.8163,0.0305),rnorm(1,-2.66053,0.0302))
+  #Draw stage I to III survival parameters
+  survmvn<-data.frame(c(-5.46208,-5.2077,-5.8016),c(-3.8163,-3.75901,-3.8811),c(-2.72264,-2.66053,-2.78617))
+  survcovmat<-cov(survmvn)
+  survmeans<-c(survmvn[1,1],survmvn[1,2],survmvn[1,3])
+  PSA_gamma_survival<-mvrnorm(mcruns,survmeans,survcovmat)
   
-  #Metatstatic survival parameters
- # metmvn<-data.frame(c(-1.78723,-1.67922,-1.89434),c(-1.38762,-1.33512,-1.49956),c(-1.01051,-0.93338,-1.08304))
-  #metmat<-cov(metmvn)
-  #metmeans<-c(metmvn[1,1],metmvn[1,2],metmvn[1,3])
-  #PSA_meta_survival<-mvrnorm(mcruns,metmeans,metmat)
-
-  PSA_meta_survival<-c(rnorm(1,-1.78723,0.0549),rnorm(1,-1.38762,0.0268),rnorm(1,-1.01051,0.0380))
+  # Draw Metatstatic survival parameters
+  metmvn<-data.frame(c(-1.78723,-1.67922,-1.89434),c(-1.38762,-1.33512,-1.49956),c(-1.01051,-0.93338,-1.08304))
+  metmat<-cov(metmvn)
+  metmeans<-c(metmvn[1,1],metmvn[1,2],metmvn[1,3])
+  PSA_meta_survival<-mvrnorm(mcruns,metmeans,metmat)
     
-  #Mammography with sensitivity conditional on tumour diameter parameters W-F
+  #Draw Mammography with sensitivity conditional on tumour diameter parameters W-F
   PSA_beta1 <- rnorm(mcruns,1.47,0.1)
   PSA_beta2 <- rnorm(mcruns,6.51,0.5)
   
-  #Mammography sensitivity by volpara density grade from PREVENTICON
+  #Draw Mammography sensitivity by volpara density grade from PREVENTICON
   PSA_Sen_VDG <- data.frame(rbeta(mcruns,96,16),rbeta(mcruns,298,86),rbeta(mcruns,212,93),rbeta(mcruns,61,39))
   Sen_VDG_av <- 0.757
   
-  #Supplemental Screening
+  #Draw supplemental Screening CDRs
   PSA_MRI_cdr <- rbeta(mcruns,99.495,19799.5) #CDR for MRI in Mammo negative women (incremental)
   PSA_US_cdr <- rbeta(mcruns,35.89,11927) #CDR for US in Mammo negative women (incremental)
   
-  #Set tumour growth rate parameters
+  #Draw tumour growth rate parameters
   PSA_log_norm_mean <- rnorm(mcruns,1.07,0.09)
   PSA_log_norm_sd <- rnorm(mcruns,1.31,0.11)
   
-  #Costs
+  #Draw costs
   PSA_cost_strat<-rlnorm(mcruns,2.13387381,0.06349671)
   PSA_costvar<-rnorm(mcruns,0,0.1020408)
   PSA_costscreen<-rnorm(mcruns,0,0.1020408)
@@ -342,16 +338,16 @@ if(gensample==1){
   PSA_cost_MRI<-rnorm(mcruns,0,0.1020408)
   
   #Generate utility draws
- # utilmat<-data.frame(c(1-0.82,1-0.81,1-0.83),c(1-0.75,1-0.73,1-0.77))
-  #lnutilmat<-log(utilmat)
-  #covutil<-cov(lnutilmat)
-  #utilmeans<-c(log(1-0.82),log(1-0.75))
-  #PSA_util<-1-exp(mvrnorm(mcruns,utilmeans,covutil))
+  utilmat<-data.frame(c(1-0.82,1-0.81,1-0.83),c(1-0.75,1-0.73,1-0.77))
+  lnutilmat<-log(utilmat)
+  covutil<-cov(lnutilmat)
+  utilmeans<-c(log(1-0.82),log(1-0.75))
+  PSA_util<-1-exp(mvrnorm(mcruns,utilmeans,covutil))
   
-  PSA_util<-c(rnorm(1,0.82,0.00510),rnorm(1,0.75,0.0102))
-  
+  #Generate id for monte carlo set
   mcid<-c(1:mcruns)
   
+  #Bind monte carlo draws
   PSA_all_p<-cbind(PSA_gamma_survival,PSA_meta_survival,PSA_beta1,PSA_beta2,
                    PSA_Sen_VDG,PSA_MRI_cdr,PSA_US_cdr,PSA_log_norm_mean,
                    PSA_log_norm_sd,PSA_cost_strat,PSA_costvar,PSA_util,PSA_costscreen,
@@ -365,16 +361,19 @@ if(gensample==1){
                          "PSA_util_1to3","PSA_util_4","PSA_costscreen","PSA_cost_follow_up",
                          "PSA_cost_biop","PSA_cost_US","PSA_cost_MRI","mcid")
   
-masterframe<-data.frame(matrix(nrow=inum*mcruns,ncol=length(risksample[1,])+length(PSA_all_p[1,])))
-masterframe[,1:14]<-risksample
-masterframe[,15:40]<-PSA_all_p
-masterframe[,41]<-(rep(1:chunks,times=round(length(masterframe[,1])/chunks)))
-masterframe<-masterframe %>% filter(masterframe[,11]>=50)
-risksplit<-split(masterframe,masterframe[,41])
-rm(masterframe,risksample,PSA_all_p,risk_mat)
+  #Bind individual level parameters and monte carlo draws
+  masterframe<-data.frame(matrix(nrow=inum*mcruns,ncol=length(risksample[1,])+length(PSA_all_p[1,])))
+  masterframe[,1:14]<-risksample
+  masterframe[,15:40]<-PSA_all_p
+  
+  #Split the dataframe into chunks for easier computation
+  masterframe[,41]<-(rep(1:chunks,times=round(length(masterframe[,1])/chunks)))
+  masterframe<-masterframe %>% filter(masterframe[,11]>=50)
+  risksplit<-split(masterframe,masterframe[,41])
+  rm(masterframe,risksample,PSA_all_p,risk_mat)
 
-#Save risk sample in chunks
-for(i in 1:chunks){
+  #Save risk sample in chunks
+  for(i in 1:chunks){
   splitsample<-as.data.frame(risksplit[i])
   save(splitsample,file = paste("Risksample/risksample_",i,".Rdata",sep=""))
 }
