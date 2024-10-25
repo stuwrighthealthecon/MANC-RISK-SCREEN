bc5bfdbab33fa868014e43cea439d872a1f371ab
Joe Hilton <joe.b.hilton@gmail.com>
2024-10-25 17:00:42 +0100

    Added synthetic population with true/estimated risks

--- Model Core Files/explore_misclassification.R
+++ Model Core Files/explore_misclassification.R
@@ -74,8 +74,8 @@ cdf_df <- data.frame("data_quantiles"=data_quantiles,
                           "heights"=heights,
                           "beta_cdf"=beta_cdf)
 h <- ggplot(cdf_df, aes(x=heights, y=data_quantiles)) +
-  geom_line(colour="red") +
-  geom_point(aes(x=heights, y=beta_cdf)) +
+  geom_point() +
+  geom_line(aes(x=heights, y=beta_cdf), colour="red") +
   scale_x_continuous(transform = "log10")
 
 # Should see from plot that fit is fairly poor!
@@ -97,39 +97,40 @@ cdf_df <- data.frame("data_quantiles"=data_quantiles,
                           "heights"=heights,
                           "beta_cdf"=beta_cdf)
 h <- ggplot(cdf_df, aes(x=heights, y=data_quantiles)) +
-  geom_line(colour="red") +
-  geom_point(aes(x=heights, y=beta_cdf)) +
-  scale_x_continuous(transform = "log10")
+  geom_point() +
+  geom_line(aes(x=heights, y=beta_cdf), colour="red") +
+  scale_x_continuous(transform = "log10") +
+  ggtitle(supp_data$Model_age[1])
 
 # Fit now looks much better!
 print(h)
-
-# This fit does not preserve true mean:
-estim_mean <- par_estim$par[1] / (par_estim$par[1] + par_estim$par[2])
-true_mean <- supp_data_tc50$O[1] / supp_data_tc50$N[1]
-
-# Alternative approach: force fits to match mean
-get_beta_rmse_with_mean <- function(alpha){
-  beta <- alpha / true_mean - alpha
-  cdf <- pbeta(heights, alpha, beta)
-  rmse <- (1/length(data_quantiles)) * (data_quantiles - cdf)^2 %>% sum() %>% sqrt()
-  return(rmse)
-}
-par_estim <- optim(alpha_vals[1], get_beta_rmse_with_mean)
-alpha_estim <- par_estim$par[1]
-beta_estim <- alpha_estim / true_mean - alpha_estim
-estim_mean <- alpha_estim / (alpha_estim + beta_estim)
-
-beta_cdf <- pbeta(heights, alpha_estim, beta_estim)
-cdf_df <- data.frame("data_quantiles"=data_quantiles,
-                          "heights"=heights,
-                          "beta_cdf"=beta_cdf)
-# Should see much worse eyeball fit:
-h <- ggplot(cdf_df, aes(x=heights, y=data_quantiles)) +
-  geom_line(colour="red") +
-  geom_point(aes(x=heights, y=beta_cdf)) +
-  scale_x_continuous(transform = "log10")
-print(h)
+# 
+# # This fit does not preserve true mean:
+# estim_mean <- par_estim$par[1] / (par_estim$par[1] + par_estim$par[2])
+# true_mean <- supp_data_tc50$O[1] / supp_data_tc50$N[1]
+# 
+# # Alternative approach: force fits to match mean
+# get_beta_rmse_with_mean <- function(alpha){
+#   beta <- alpha / true_mean - alpha
+#   cdf <- pbeta(heights, alpha, beta)
+#   rmse <- (1/length(data_quantiles)) * (data_quantiles - cdf)^2 %>% sum() %>% sqrt()
+#   return(rmse)
+# }
+# par_estim <- optim(alpha_vals[1], get_beta_rmse_with_mean)
+# alpha_estim <- par_estim$par[1]
+# beta_estim <- alpha_estim / true_mean - alpha_estim
+# estim_mean <- alpha_estim / (alpha_estim + beta_estim)
+# 
+# beta_cdf <- pbeta(heights, alpha_estim, beta_estim)
+# cdf_df <- data.frame("data_quantiles"=data_quantiles,
+#                           "heights"=heights,
+#                           "beta_cdf"=beta_cdf)
+# # Should see much worse eyeball fit:
+# h <- ggplot(cdf_df, aes(x=heights, y=data_quantiles)) +
+#   geom_point() +
+#   geom_line(aes(x=heights, y=beta_cdf), colour="red") +
+#   scale_x_continuous(transform = "log10")
+# print(h)
 
 #### Do least-squares beta regression systematically for each age/model combination
 
@@ -148,10 +149,12 @@ beta_fits_df <- data.frame("obs_mean"=numeric(length=n_classes),
 for (m in unique(supp_data$Model_age)){
   supp_data_m <- supp_data[which(supp_data$Model_age==m), ]
   data_quantiles <- cumsum(supp_data_m$N[2:6])/ supp_data_m$N[1]
+  print(data_quantiles)
   
   # First do expected
-  heights <- supp_data_m$E[2:6] / supp_data_m$N[2:6]
-  heights[5] <- 1
+  heights <- c(supp_data_m$E[2:5] / supp_data_m$N[2:5], 1)
+  print(heights)
+  # heights[5] <- 1
   get_beta_rmse <- function(pars){
     alpha <- pars[1]
     beta <- pars[2]
@@ -159,25 +162,30 @@ for (m in unique(supp_data$Model_age)){
     rmse <- (1/length(data_quantiles)) * (data_quantiles - cdf)^2 %>% sum() %>% sqrt()
     return(rmse)
   }
-  alpha_0 <- supp_data_m$E[which(supp_data_m$Risk_class=="Total")]
-  beta_0 <- supp_data_m$N[which(supp_data_m$Risk_class=="Total")] - supp_data_m$E[which(supp_data_m$Risk_class=="Total")]
+  
+  alpha_0 <- supp_data_m$E[which(supp_data_m$Risk_class=="Total")] / supp_data_m$N[which(supp_data_m$Risk_class=="Total")] 
+  beta_0 <- 1 - alpha_0
   par_estim_e <- optim(c(alpha_0, beta_0), get_beta_rmse)
   
-  beta_cdf <- pbeta(heights, par_estim_e$par[1], par_estim_e$par[2])
-  cdf_df <- data.frame("data_quantiles"=data_quantiles,
-                       "heights"=heights,
+  X <- seq(0, max(heights), by=.001)
+  
+  beta_cdf <- pbeta(X, par_estim_e$par[1], par_estim_e$par[2])
+  
+  data_df <- data.frame("heights"=heights,
+                        "data_quantiles"=data_quantiles)
+  cdf_df <- data.frame("X"=X,
                        "beta_cdf"=beta_cdf)
   
-  h <- ggplot(cdf_df, aes(x=heights, y=data_quantiles)) +
-    geom_line(colour="red") +
-    geom_point(aes(x=heights, y=beta_cdf)) +
+  h <- ggplot() +
+    geom_point(data=data_df, aes(x=heights, y=data_quantiles)) +
+    geom_line(data=cdf_df, aes(x=X, y=beta_cdf), colour="red") +
     scale_x_continuous(transform = "log10") +
     ggtitle(paste(m, ", expected", sep=""))
   print(h)
   
   # Now do estimates
-  heights <- supp_data_m$O[2:6] / supp_data_m$N[2:6]
-  heights[5] <- 1
+  heights <- c(supp_data_m$O[2:5] / supp_data_m$N[2:5], 1)
+  # heights[5] <- 1
   get_beta_rmse <- function(pars){
     alpha <- pars[1]
     beta <- pars[2]
@@ -185,18 +193,22 @@ for (m in unique(supp_data$Model_age)){
     rmse <- (1/length(data_quantiles)) * (data_quantiles - cdf)^2 %>% sum() %>% sqrt()
     return(rmse)
   }
-  alpha_0 <- supp_data_m$O[which(supp_data_m$Risk_class=="Total")]
-  beta_0 <- supp_data_m$N[which(supp_data_m$Risk_class=="Total")] - supp_data_m$O[which(supp_data_m$Risk_class=="Total")]
+  alpha_0 <- supp_data_m$O[which(supp_data_m$Risk_class=="Total")] / supp_data_m$N[which(supp_data_m$Risk_class=="Total")] 
+  beta_0 <- 1 - alpha_0
   par_estim_o <- optim(c(alpha_0, beta_0), get_beta_rmse)
   
-  beta_cdf <- pbeta(heights, par_estim_o$par[1], par_estim_o$par[2])
-  cdf_df <- data.frame("data_quantiles"=data_quantiles,
-                       "heights"=heights,
+  X <- seq(0, max(heights), by=.001)
+  
+  beta_cdf <- pbeta(X, par_estim_o$par[1], par_estim_o$par[2])
+  
+  data_df <- data.frame("heights"=heights,
+                        "data_quantiles"=data_quantiles)
+  cdf_df <- data.frame("X"=X,
                        "beta_cdf"=beta_cdf)
   
-  h <- ggplot(cdf_df, aes(x=heights, y=data_quantiles)) +
-    geom_line(colour="red") +
-    geom_point(aes(x=heights, y=beta_cdf)) +
+  h <- ggplot() +
+    geom_point(data=data_df, aes(x=heights, y=data_quantiles)) +
+    geom_line(data=cdf_df, aes(x=X, y=beta_cdf), colour="red") +
     scale_x_continuous(transform = "log10") +
     ggtitle(paste(m, ", observed", sep=""))
   print(h)
@@ -217,6 +229,9 @@ save(beta_fits_df, file="fitted_risk_distributions.csv")
 #### Next problem: inferring life risk from ten year risk
 
 risk_mat<-read.csv("synthetic_risk_data.csv") %>% dplyr::select(-"X")
+
+risk_mat$syn.X10yr <- .01 * risk_mat$syn.X10yr # Rescale to numbers rather than percentages
+risk_mat$syn.life <- .01 * risk_mat$syn.life
 
 # Inspect visually:
 
@@ -240,7 +255,9 @@ p
 # Should see obvious difference in relationship, so need to include age in regression model
 library(GauPro)
 
-risk_mat$bin.10yr <- risk_mat$syn.X10yr %>% cut(breaks=seq(from=0, to=100, by=.1))
+risk_mat$bin.10yr <- risk_mat$syn.X10yr %>% cut(breaks=seq(from=0, to=1, by=.001))
+
+# Needs to be age stratified, so try first on 50-60 group (main group of interest)
 
 smpl <- risk_mat[which(risk_mat$age_grp=="50-60"),] %>%
   slice_sample(by=bin.10yr, n=1)
@@ -255,9 +272,9 @@ y_upper <- ypred$mean + 2*ypred$se
 y_lower <- ypred$mean - 2*ypred$se
 p <- ggplot(risk_mat, aes(syn.X10yr, syn.life)) +
   geom_point() +
-  geom_line(aes(x=risk_mat$syn.X10yr, y=ypred$mean), colour="red") +
-  geom_line(aes(x=risk_mat$syn.X10yr, y=y_upper), colour="blue") +
-  geom_line(aes(x=risk_mat$syn.X10yr, y=y_lower), colour="blue")
+  geom_line(aes(x=syn.X10yr, y=ypred$mean), colour="red") +
+  geom_line(aes(x=syn.X10yr, y=y_upper), colour="blue") +
+  geom_line(aes(x=syn.X10yr, y=y_lower), colour="blue")
 print(p)
 
 # Last point is a bit of an outlier, try retraining without:
@@ -275,6 +292,48 @@ p <- ggplot(risk_mat, aes(syn.X10yr, syn.life)) +
   geom_line(aes(x=risk_mat$syn.X10yr, y=y_upper), colour="blue") +
   geom_line(aes(x=risk_mat$syn.X10yr, y=y_lower), colour="blue")
 print(p)
+
+# Rename to reflect age group
+gp_50_60 <- gp_short
+
+# Now do other age bands:
+
+smpl <- risk_mat[which(risk_mat$age_grp=="<50"),] %>%
+  slice_sample(by=bin.10yr, n=1)
+
+x <- smpl$syn.X10yr
+y <- smpl$syn.life
+kern <- Matern52$new(0)
+gp_u50 <- GauPro(x, y, kernel=kern)
+ypred <- gp_u50$predict(risk_mat$syn.X10yr, se=T)
+y_upper <- ypred$mean + 2*ypred$se
+y_lower <- ypred$mean - 2*ypred$se
+p <- ggplot(risk_mat, aes(syn.X10yr, syn.life)) +
+  geom_point() +
+  geom_line(aes(x=syn.X10yr, y=ypred$mean), colour="red") +
+  geom_line(aes(x=syn.X10yr, y=y_upper), colour="blue") +
+  geom_line(aes(x=syn.X10yr, y=y_lower), colour="blue")
+print(p)
+
+# Actually looks like it's points with >25% 10yr risk causing trouble, so this
+# might be a better rule to use if we ever do this with new risk data
+
+smpl <- smpl %>% filter(syn.X10yr<=25)
+x <- smpl$syn.X10yr
+y <- smpl$syn.life
+kern <- Matern52$new(0)
+gp_u50 <- GauPro(x, y, kernel=kern)
+ypred <- gp_u50$predict(risk_mat$syn.X10yr, se=T)
+y_upper <- ypred$mean + 2*ypred$se
+y_lower <- ypred$mean - 2*ypred$se
+p <- ggplot(risk_mat, aes(syn.X10yr, syn.life)) +
+  geom_point() +
+  geom_line(aes(x=syn.X10yr, y=ypred$mean), colour="red") +
+  geom_line(aes(x=syn.X10yr, y=y_upper), colour="blue") +
+  geom_line(aes(x=syn.X10yr, y=y_lower), colour="blue")
+print(p)
+
+gp_list <- c(gp_u50, gp_50_60)
 
 #### In this optional section we look at transformations of the data to improve the GP fit.
 # Note that the code below this section will still use gp_short, the Gaussian process
@@ -315,30 +374,30 @@ curve(exp(gp$predict(x)+ 1.1*min(y))-yse(x), add=T, col=4)
 
 #### Now add "true" risks to risk_mat, assuming we're working with prediction
 # with density.
-
+# 
 bf_dens_only <- beta_fits_df[grep("density", rownames(beta_fits_df)),]
 supp_data_dens_only <- supp_data %>%
                        filter(grepl("density", Model_age)) %>%
                        filter(grepl("Total", Risk_class))
 cutoffs <- c(0, 50, 60)
-
-# Function for synthetic to true risk mapping
-# Problem: this can generate negatives
-true_risk_from_syn <- function(syn_risk, age){
-  age_class <- which(cutoffs<age)  %>% max()
-  beta_pars <- c(bf_dens_only$alpha_o[age_class],
-                 bf_dens_only$beta_o[age_class])
-  true_risk <- 100*(syn_risk/100 * as.numeric(supp_data_dens_only$O_E[age_class]) -
-                bf_dens_only$obs_mean[age_class] + rbeta(1, beta_pars[1], beta_pars[2]) )
-  return(true_risk)
-}
-
-true_risk_from_syn_v <- Vectorize(true_risk_from_syn)
+# 
+# # Function for synthetic to true risk mapping
+# # Problem: this can generate negatives
+# true_risk_from_syn <- function(syn_risk, age){
+#   age_class <- which(cutoffs<age)  %>% max()
+#   beta_pars <- c(bf_dens_only$alpha_o[age_class],
+#                  bf_dens_only$beta_o[age_class])
+#   true_risk <- 100*(syn_risk/100 * as.numeric(supp_data_dens_only$O_E[age_class]) -
+#                 bf_dens_only$obs_mean[age_class] + rbeta(1, beta_pars[1], beta_pars[2]) )
+#   return(true_risk)
+# }
+# 
+# true_risk_from_syn_v <- Vectorize(true_risk_from_syn)
 
 # Alternative approach: fit a maximum likelihood beta distribution to synthetic
 # risk and map between quantiles
 
-# TODO: STRATIFY BY AGE
+
 beta_logpmf <- function(alpha, risk_data){
   if((alpha[[1]]>=0)&(alpha[[2]]>=0)){
     lpmf<- dbeta(.01*risk_data$syn.X10yr, alpha[[1]], alpha[[2]]) %>% log() %>% sum()
@@ -352,24 +411,29 @@ syn_beta_pars <- sapply(c("<50", "50-60"), FUN=function(str){
   }
   )
 true_risk_from_syn <- function(syn_risk, age){
-  # print(syn_risk)
+  
   age_class <- which(cutoffs<age)  %>% max()
+  # cat("Age class =",age_class,"\n")
   
   beta_pars_e <- c(bf_dens_only$alpha_e[age_class],
                    bf_dens_only$beta_e[age_class])
-  # Get cdf evaluated at synthetic risk
-  cdf <- pbeta(.01*syn_risk,
+  # cat("exp pars=",beta_pars_e,"\n")
+  beta_pars_o <- c(bf_dens_only$alpha_o[age_class],
+                   bf_dens_only$beta_o[age_class])
+  # cat("obs pars=",beta_pars_o,"\n")
+  
+  # cat("syn_risk=",syn_risk,"\n")
+  
+  # Get individual's position in estimated risk distribution
+  quantile <- pbeta(syn_risk,
                     beta_pars_e[1],
                     beta_pars_e[2])
-  # print(cdf)
-  beta_pars_o <- c(bf_dens_only$alpha_o[age_class],
-                 bf_dens_only$beta_o[age_class])
-  # print(c(syn_beta_pars[1, age_class],
-  #         syn_beta_pars[2, age_class]))
-  # print(beta_pars)
-  # Get quantile associated with same position along true risk distribution:
-  true_risk <- 100*qbeta(cdf, beta_pars_o[1], beta_pars_o[2])
-  # print(pbeta(.01*true_risk, beta_pars[1], beta_pars[2]))
+  # cat("quantile=",quantile,"\n")
+  
+  # Get risk at same position in observed risk distribution:
+  true_risk <- qbeta(quantile, beta_pars_o[1], beta_pars_o[2])
+  # cat("qbeta=",pbeta(true_risk, beta_pars_o[1], beta_pars_o[2]),"\n")
+  # cat("true_risk=",true_risk,"\n")
   return(true_risk)
 }
 
@@ -378,13 +442,76 @@ true_risk_from_syn_v <- Vectorize(true_risk_from_syn)
 # Create copy of risk_mat with misclassification
 risk_mat_with_mc <- risk_mat %>%
                     mutate(syn.X10yr.true = true_risk_from_syn_v(syn.X10yr,
-                                                                 syn.Age)) %>%
-                    mutate(syn.life.true.dist = gp_short$predict(syn.life,
-                                                                 se=T)) %>%
-                    mutate(syn.life.true = rnorm(nrow(risk_mat), mean=syn.life.true.dist$mean,
-                                                 sd = syn.life.true.dist$se)) %>%
-                    select(-syn.life.true.dist)
+                                                                 syn.Age))# %>%
+                    # mutate(syn.life.true.dist = gp_short$predict(syn.life,
+                    #                                              se=T)) %>%
+                    # mutate(syn.life.true = rnorm(nrow(risk_mat), mean=syn.life.true.dist$mean,
+                    #                              sd = syn.life.true.dist$se)) %>%
+                    # select(-syn.life.true.dist)
 
 # Plot to check:
 
 ggplot(risk_mat_with_mc, aes(syn.X10yr, syn.X10yr.true, col=age_grp)) + geom_point() + geom_abline()
+
+# Quick exploration of mapping between risks:
+
+X <- seq(0,1,.0001)
+Ye <- pbeta(X,
+            bf_dens_only$alpha_e[1],
+            bf_dens_only$beta_e[1])
+Yo <- pbeta(X,
+            bf_dens_only$alpha_o[1],
+            bf_dens_only$beta_o[1])
+plot(Ye, Yo)
+
+Ye <- pbeta(X,
+            bf_dens_only$alpha_e[2],
+            bf_dens_only$beta_e[2])
+Yo <- pbeta(X,
+            bf_dens_only$alpha_o[2],
+            bf_dens_only$beta_o[2])
+plot(Ye, Yo)
+
+# Should see that problematic cases are all ones with high risk
+problem_cases <- risk_mat_with_mc %>% filter(syn.X10yr.true==1)
+print(problem_cases$syn.X10yr)
+
+# Try switching to linear interpolation for these given plots look close to linear:
+linint_df <- risk_mat_with_mc %>%
+            filter(age_grp=="50-60") %>%
+            dplyr::select(syn.X10yr, syn.X10yr.true) %>%
+            filter(!syn.X10yr.true==1)
+lmodel <- lm(linint_df$syn.X10yr.true ~ linint_df$syn.X10yr)
+
+# Try replacing problem cases with linear extrapolated values:
+problem_locs <- which(risk_mat_with_mc$syn.X10yr.true==1)
+risk_mat_with_mc$syn.X10yr.true[problem_locs] <- problem_cases$syn.X10yr * lmodel$coefficients[2]
+
+# Should look more reasonable now
+ggplot(risk_mat_with_mc, aes(syn.X10yr, syn.X10yr.true, col=age_grp)) + geom_point() + geom_abline()
+
+# Now add life risks:
+
+ypred <- gp_short$predict(risk_mat_with_mc$syn.X10yr.true, se=T)
+y_upper <- ypred$mean + 2*ypred$se
+y_lower <- ypred$mean - 2*ypred$se
+
+get_liferisk <- function(age_grp_idx, syn.X10yr){
+  gp_i <- gp_list[[age_grp_idx]]
+  return(gp_i$predict(syn.X10yr))
+}
+
+get_liferisk <- Vectorize(get_liferisk)
+
+risk_mat_with_mc <- risk_mat_with_mc %>%
+                    mutate(age_grp_idx = ifelse(age_grp=="<50",1,2)) %>%
+                    mutate(syn.life.true = get_liferisk(age_grp_idx, syn.X10yr.true))
+
+# Check life risk plots:
+ggplot(risk_mat_with_mc, aes(syn.life, syn.life.true, col=age_grp)) + geom_point() + geom_abline()
+risk_mat_with_mc <- risk_mat_with_mc %>%
+                    dplyr::select(-age_grp) %>%
+                    dplyr::select(-age_grp_idx) %>%
+                    dplyr::select(-bin.10yr)
+
+write.csv(risk_mat_with_mc, "synthetic_risk_data_with_misclassification.csv")
