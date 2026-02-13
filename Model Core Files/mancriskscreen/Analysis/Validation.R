2e3f7e3fbc30df1f5ed40a0e8b9d042027c61d50
Stuart Wright <82210105+stuwrighthealthecon@users.noreply.github.com>
2026-02-13 10:55:44 +0000

    update validation

--- Model Core Files/mancriskscreen/Analysis/Validation.R
+++ Model Core Files/mancriskscreen/Analysis/Validation.R
@@ -1,32 +1,12 @@
-screen_strategy <- 3
-load(paste(
-  "Deterministic results/misclassification_and_preventative_drug/Determ_",
-  screen_strategy,
-  "_",
-  1,
-  ".Rdata",
-  sep = ""
-))
+filenames <- list.files(det_output_path, full.names = TRUE)
+alldata <- lapply(filenames, function(x) {
+  get(load(x, .GlobalEnv))
+})
+alldata <- do.call("rbind", alldata)
 
 #Remove women with cancer diagnosed before age 50
-results <- results %>% filter(results[, 4] > 50 | results[, 4] == 0)
-detresults <- results
-
-for (i in 2:10) {
-  #name of saved files needed
-  load(paste(
-    "Deterministic results/misclassification_and_preventative_drug/Determ_",
-    screen_strategy,
-    "_",
-    i,
-    ".Rdata",
-    sep = ""
-  ))
-
-  #Remove women with cancer diagnosed before age 50
-  results <- results %>% filter(results[, 4] > 50 | results[, 4] == 0)
-  detresults <- rbind(detresults, results)
-}
+alldata <- alldata %>% filter(alldata[, 4] > 50 | alldata[, 4] == 0)
+detresults <- alldata
 
 screendet <- detresults %>% filter(detresults$`screen detected` == 1)
 clindet <- detresults %>% filter(detresults$`screen detected` == 0)
