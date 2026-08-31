436d127cf48635e98ca65389cfe6b8978e2fc395
Stuart Wright <82210105+stuwrighthealthecon@users.noreply.github.com>
2026-08-31 15:26:25 +0100

    update

--- Model Core Files/mancriskscreen/MANC-RISK-SCREEN fast parallelised.R
+++ Model Core Files/mancriskscreen/MANC-RISK-SCREEN fast parallelised.R
@@ -20,10 +20,10 @@ controls <- list(
   MISCLASS         = TRUE, #should error in risk prediction be included?
   PREVENTATIVE_DRUG = FALSE, #should risk reducing medicines be used for high risk?
   supplemental_screening = FALSE, #should ultrasound and MRI be used as supplemental screening?
-  PSA              = TRUE, #run PSA?
-  intervals        = TRUE, #run PSA with wide distributions for GAM estimation?
-  desired_cases    = 1, #number of cancer cases required
-  mcruns           = 10000, #Number of Monte Carlo runs
+  PSA              = FALSE, #run PSA?
+  intervals        = FALSE, #run PSA with wide distributions for GAM estimation?
+  desired_cases    = 300000, #number of cancer cases required
+  mcruns           = 1, #Number of Monte Carlo runs
   seed             = 42, #Set seed for random number generation
   n_cores          = max(1L, parallel::detectCores() - 1L) #Select number of computer cores to use
 )
